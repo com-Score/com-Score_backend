6f68df4ca3b808a14c6158fc772a552e18cc8fb8
minseong1 <alstjd2627@naver.com>
2024-08-12 18:26:54 +0900

    feat: create ApiResponse

---
+++ src/main/java/comscore/backend/global/response/ApiResponse.java
@@ -0,0 +1,30 @@
+package comscore.backend.global.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ApiResponse< T > {
+  private String message;
+  private T data;
+
+  public ApiResponse( String message, T data) {
+    this.message = message;
+    this.data = data;
+  }
+
+  public static < T > ApiResponse< T > ok( String message, T data ) {
+    return new ApiResponse< T >(
+        message,
+        data
+    );
+  }
+
+  public static < T > ApiResponse< T > fail( String message, T errorResponse ) {
+    return new ApiResponse< T >(
+        message,
+        errorResponse
+    );
+  }
+}
